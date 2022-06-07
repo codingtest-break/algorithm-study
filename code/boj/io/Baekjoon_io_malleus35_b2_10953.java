fb5ccf4dbef38540542732d8fe16e8089c65c7db
Junghun Yang <junghun.yang.0914@linecorp.com>
2022-06-08 02:39:28 +0900

    IO-malleus35-1 Java 기본 입출력 문제 해결

---
+++ code/boj/io/Baekjoon_io_malleus35_b2_10953.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            System.out.println(Arrays.stream(br.readLine().split(",")).mapToInt(e -> Integer.parseInt(e))
+                    .reduce((x, y) -> x + y).getAsInt());
+        }
+    }
+}
